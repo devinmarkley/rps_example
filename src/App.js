9e6dccc5c3737f31fcf6d95e92d297037c9f04ac
Devin Markley <devin.markley@trojans.dsu.edu>
2024-09-26 12:09:05 -0400

    example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import "./styles.css";
 import React from "react";
 import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
-import image  from "./images/somePano.JPG"
+import image from "./images/somePano.JPG";
 
 function App() {
-    const photoSphereRef = React.useRef();
-    return (
+  const photoSphereRef = React.useRef();
+  return (
     <div className="App">
-        <ReactPhotoSphereViewer
-            ref={photoSphereRef}
-            // src={"Test_Pano.jpg"}
-            src={image}
-            defaultZoomLvl={0}
-            hideNavbarButton={true}
-            width={"100vw"}
-            height={"100vh"}
-            navbar={false}
-        />
+      <ReactPhotoSphereViewer
+        ref={photoSphereRef}
+        src={image}
+        defaultZoomLvl={0}
+        hideNavbarButton={true}
+        width={"100vw"}
+        height={"100vh"}
+        navbar={false}
+      />
     </div>
   );
 }
